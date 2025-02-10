9b71bd7593faec293fbca0fa2f5a1abc1c4eb3f3
Andrew Grant <atgrant23@gmail.com>
2025-02-10 06:59:55 -0500

    Made the menu more appealing

--- src/App.java
+++ src/App.java
@@ -2,11 +2,12 @@ import java.util.Scanner;
 
 import BasicExamples.*;
 import IntermediateExamples.*;
+import AdvancedExamples.*;
 
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //System.out.println("Hello, World!");
 
         //BasicExampleMethods();
         IntermediateExampleMethods();
@@ -46,19 +47,22 @@ public class App {
 
     public static void IntermediateExampleMethods() {        
         int userChoice = 0;
-        System.out.println("Intermediate Examples Menu");
-        System.out.println("01. Madlibs Game Example");
-        System.out.println("02. Shopping Cart Example");
-        System.out.println("03. Calculator Example");
-        System.out.println("04. Weight Conversion Example");
-        System.out.println("05. Temperature Conversion Example");
-        System.out.println("06. Enhanced Calculator Example");
-        System.out.println("07. Number Guessing Game Example");
-        System.out.println("08. Banking Program Example");
-        System.out.println("09. Dice Roller Example");
-        System.out.println("10. Quiz Game Example");
-        System.out.println("11. Rock Paper Scissors Game Example");
-        System.out.println("12. Slot Machine Program Example");
+        System.out.println("|---------------------------------------|");
+        System.out.println("|      Intermediate Examples Menu       |");
+        System.out.println("|---------------------------------------|");
+        System.out.println("|01| - Madlibs Game Example             |");
+        System.out.println("|02| - Shopping Cart Example            |");
+        System.out.println("|03| - Calculator Example               |");
+        System.out.println("|04| - Weight Conversion Example        |");
+        System.out.println("|05| - Temperature Conversion Example   |");
+        System.out.println("|06| - Enhanced Calculator Example      |");
+        System.out.println("|07| - Number Guessing Game Example     |");
+        System.out.println("|08| - Banking Program Example          |");
+        System.out.println("|09| - Dice Roller Example              |");
+        System.out.println("|10| - Quiz Game Example                |");
+        System.out.println("|11| - Rock Paper Scissors Game Example |");
+        System.out.println("|12| - Slot Machine Program Example     |");
+        System.out.println("|---------------------------------------|");
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Which program would you like to run: ");
@@ -83,6 +87,10 @@ public class App {
         System.out.println("Closing Intermediate Examples...");
 
         scanner.close();
+    }
+
+    public static void AdvancedExampleMethods() {
+
     }
 
     public static void MadLibsGameExample() {
