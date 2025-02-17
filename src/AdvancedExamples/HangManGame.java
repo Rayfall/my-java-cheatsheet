13c67469b0cb8970dce4449c746a7888aabc045e
Andrew Grant <atgrant23@gmail.com>
2025-02-16 21:41:54 -0500

    Started Hang Man Game

--- src/AdvancedExamples/HangManGame.java
+++ src/AdvancedExamples/HangManGame.java
@@ -1,5 +1,16 @@
 package AdvancedExamples;
 
+import java.util.Scanner;
+
 public class HangManGame {
+    String word = "light";
+
+    public void Game() {
+        Scanner scanner = new Scanner(System.in);
+
+
+        scanner.close();
+
+    }
     
 }
